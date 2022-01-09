62e5a99cdf333e35c26b950311eaca5905f83c6b
unknown <n.rahnama1996@gmail.com>
2022-01-09 07:45:38 +0330

    folder

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -1,17 +1,8 @@
-import {Fragment} from 'react'
 
-
-import Header from "./components/Layout/Header";
-import Meals from './components/Meals/Meals'
 
 function App() {
   return (
-  <Fragment>
-    <Header/>
-    <main>
-      <Meals/>
-    </main>
-  </Fragment>
+  
   );
 }
 
